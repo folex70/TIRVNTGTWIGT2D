7f42f190349d172fa1155e98d65d0bd28c7185a2
J G <J G>
2016-06-19 22:56:31 -0300

    primeiro commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float vel;
+	public Rigidbody2D rb;
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		vel = 6;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//vx = vel * Input.GetAxis ("Horizontal");
+		//rb.velocity.x = vx;
+
+		Vector2 temp = rb.velocity;
+		temp.x = vel * Input.GetAxis("Horizontal");
+		rb.velocity = temp;
+
+
+		Debug.Log (Input.GetAxis("Horizontal"));
+		Debug.Log ("vel do rb "+rb.velocity.x);
+
+	}
+}
